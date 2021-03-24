3f1ce735827e0ed80e338756bff5e4421117aff0
Brian Lemons <brian_lemons@hotmail.com>
2021-03-24 14:08:50 -0700

    here ya go Jacob

--- server.js
+++ server.js
@@ -10,15 +10,32 @@ const cors = require('cors');
 
 const weather = require('./data/weather.json');
 
+
 app.use(cors());
 
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`listening on ${port}`));
+app.get('/weather', handleWeather);
 
-app.get('/weather', function (request, response) {
-  response.send(weather);
+app.use('*', (req, res) => {
+  res.status(404).send('404 Page Not Found');
 });
 
-console.log(`${weather.lat} and ${weather.lon}`);
+function handleWeather(req, res) {
+  const forcastArr = weather.data.map(day => {
+    return new Forecast(day, weather.city_name, weather.lat, weather.lon);
+  });
+  res.status(200).send(forcastArr);
+}
 
+function Forecast(obj, city, lat, lon) {
+  this.description = obj.weather.description;
+  this.date = obj.datetime;
+  this.city = city;
+  this.lat = lat;
+  this.lon = lon;
+}
+
+
+
+app.listen(port, () => console.log(`listening on ${port}`));
