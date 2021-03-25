a14744adcfcf5945d1d732415f0cba20e44bf132
Brian Lemons <brian_lemons@hotmail.com>
2021-03-24 18:48:42 -0700

    adding lab 08 stuff

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ require('dotenv').config();
 
 const cors = require('cors');
 
-const weather = require('./data/weather.json');
-
+const port = process.env.PORT;
 
 app.use(cors());
 
-const port = process.env.PORT;
+const superagent = require('superagent');
+
+//  serv start
+// ```
+// ```
+// ```
+
+// const weather = require('./data/weather.json');
 
 app.get('/weather', handleWeather);
 
@@ -22,19 +28,40 @@ app.use('*', (req, res) => {
 });
 
 function handleWeather(req, res) {
-  const forcastArr = weather.data.map(day => {
-    return new Forecast(day, weather.city_name, weather.lat, weather.lon);
-  });
-  res.status(200).send(forcastArr);
+  const { lat, lon} = req.query;
+  console.log(lat, lon, process.env.WEATHER_API_KEY);
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat={lat}&lon={lon}&key={process.env.WEATHER_API_KEY}&days=8`;
+
+  superagent
+    .get(url)
+    .query(lat, lon)
+    .then(superagentResults => {
+      const newForcast = superagentResults.body.map(agent => {
+        return new Forecast(agent);
+      });
+      res.status(200).send(newForcast);
+    });
+
 }
 
-function Forecast(obj, city, lat, lon) {
+
+function Forecast(obj, city) {
   this.description = obj.weather.description;
   this.date = obj.datetime;
   this.city = city;
-  this.lat = lat;
-  this.lon = lon;
 }
+
+
+
+//   try{
+//     const forcastArr = weather.data.map(day => {
+//       return new Forecast(day, weather.city_name, weather.lat, weather.lon);
+//     });
+//     res.status(200).send(forcastArr);
+//   } catch(error){
+//     response.status(500).send('oops! Somethings went wrong');
+//   }
+// }
 
 
 
