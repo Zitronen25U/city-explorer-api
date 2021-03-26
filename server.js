f7aa760b7b41d9d63466ac331b291de4344660e6
Brian Lemons <brian_lemons@hotmail.com>
2021-03-26 14:41:08 -0700

    refactored, and setup for lab 10

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ const cors = require('cors');
 const port = process.env.PORT;
 
 app.use(cors());
-const superagent = require('superagent');
 
 //  serv start
 // ```
 // ```
 // ```
 
-// const weather = require('./data/weather.json');
+const handleWeather = require('./modules/handleWeather');
 
 app.get('/weather', handleWeather);
 
@@ -29,38 +28,6 @@ app.get('/', (req, res) =>{
 app.use('*', (req, res) => {
   res.status(404).send('404 Page Not Found');
 });
-
-
-
-function handleWeather(req, res) {
-  const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
-  console.log(req.query);
-  const query = {
-    key: process.env.WEATHER_API_KEY,
-    lat: req.query.lat,
-    lon: req.query.lon
-  };
-
-
-  superagent
-    .get(url)
-    .query(query)
-    .then(superagentResults => {
-      const newResults = superagentResults.body.data;
-      const finalForecast = newResults.map(day => {
-        return new Forecast(day);
-      });
-      res.status(200).send(finalForecast);
-    });
-}
-
-
-function Forecast(obj, city) {
-  this.description = obj.weather.description;
-  this.date = obj.datetime;
-  this.city = city;
-}
-
 
 
 app.listen(port, () => console.log(`listening on #${port}`));
